b445f7583383e49e1721bb78d11dcf24f428b348
Michael <hovgaard1990@gmail.com>
2022-10-18 12:31:43 +0200

    Update ModalTop.jsx

--- tavernapp/src/components/ModalTop.jsx
+++ tavernapp/src/components/ModalTop.jsx
@@ -6,7 +6,7 @@ import Modal from '@mui/material/Modal';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const style = {
   position: 'absolute',
-  top: '10%',
+  top: '16%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: '50%',
@@ -19,6 +19,16 @@ const style = {
 const btnStyled = {
 color: 'red',
 };
+
+
+const liStyled ={
+    color: 'red',
+    '&:hover': {
+        backgroundColor: 'red',
+        color: 'white',
+    }
+}
+
 export default function BasicModal() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -37,11 +47,16 @@ export default function BasicModal() {
           <Typography id="modal-modal-title" variant="h6" component="h2" sx={{textAlign: 'center'}}>
             Profil
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            LOG UD! <br/>
-            Rediger profil <br/>
-            Support
+          <Typography id="modal-modal-description" sx={liStyled}>
+            LOG UD! 
           </Typography>
+          <Typography id="modal-modal-description" sx={liStyled}>
+            Rediger profil 
+          </Typography>
+          <Typography id="modal-modal-description" sx={liStyled}>
+            Support 
+          </Typography>
+
         </Box>
       </Modal>
     </div>
