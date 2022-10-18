0db7446d039c489101ab9e3485227c39526bafd9
Michael <hovgaard1990@gmail.com>
2022-10-18 11:02:56 +0200

    UPDATED

--- tavernapp/src/App.js
+++ tavernapp/src/App.js
@@ -7,17 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hej Michael!! Heiiiij |Hejsa med digsa
-        </p>
-        <h2>Magnus er en spejderdreng!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
+        
+        <h1>Magnus er en spejderdreng!</h1>
+        
       </header>
     </div>
   );
