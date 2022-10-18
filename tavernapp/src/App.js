e9e93eeb6ef77f183a3f473a049efc7f89682803
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-10-18 10:45:54 +0200

    hej

--- tavernapp/src/App.js
+++ tavernapp/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hej Michael!! Heiiiij
+          Edit <code>src/App.js</code> and save to reload. Hej Michael!! Heiiiij |Hejsa med digsa
         </p>
         <a
           className="App-link"
