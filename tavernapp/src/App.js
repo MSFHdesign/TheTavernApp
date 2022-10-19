668646f493da3abc4265c98cc060ac53232a6173
Michael <hovgaard1990@gmail.com>
2022-10-19 12:41:12 +0200

    Update App.js

--- tavernapp/src/App.js
+++ tavernapp/src/App.js
@@ -10,7 +10,6 @@ import Home from './pages/Home'
 import Login from './pages/Login';
 import TopNav from './components/TopNav';
 
-// import Login from './components/Login.js';
 
 function App() {
   return (
