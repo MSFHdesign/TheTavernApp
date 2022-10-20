741cbdd4233164e7123eaaaee3521dd0b73b33db
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-10-20 08:48:02 +0200

    Update DeleteArticle.jsx

--- tavernapp/src/components/DeleteArticle.jsx
+++ tavernapp/src/components/DeleteArticle.jsx
@@ -7,16 +7,10 @@ import { db, storage } from '../firebaseConfig'
 export default function DeleteArticle({id, imageUrl}) {
     const handleDelete =async() =>{
         try{
-          const storageRef = ref(storage,imageUrl)
-          if(storageRef==="https://firebasestorage.googleapis.com/v0/b/the-tavern-8aaf4.appspot.com/o/images%2F252297749_595594128348974_7081586526780283770_n.jpg?alt=media&token=16071e12-d653-41d8-bfe5-7a7534870801"){
-            await deleteDoc(doc(db, "Articles",id))
-            toast("article deleted successfully", {type:"success"})    
-          }else{
         await deleteDoc(doc(db, "Articles",id))
         toast("article deleted successfully", {type:"success"})
         const storageRef = ref(storage,imageUrl)
         await deleteObject(storageRef)
-      }
     }
     catch (error){
         toast("Error deleting article", {type: "error"})
