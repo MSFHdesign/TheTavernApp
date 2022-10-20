55002d2cbda9b23f080b2f57afaeb308d1392265
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-10-20 08:57:48 +0200

    more firebase

---
+++ tavernapp/src/components/DeleteProfile.jsx
@@ -0,0 +1,25 @@
+import { deleteDoc,doc } from 'firebase/firestore'
+import { deleteObject, ref } from 'firebase/storage'
+import React from 'react'
+import { toast } from 'react-toastify'
+import { db, storage } from '../firebaseConfigv2'
+
+export default function DeleteProfile({id, imageUrl}) {
+  const handleDelete =async() =>{
+      try{
+      await deleteDoc(doc(db, "profiles",id))
+      toast("profile deleted successfully", {type:"success"})
+      const storageRef = ref(storage,imageUrl)
+      await deleteObject(storageRef)
+  }
+  catch (error){
+      toast("Error deleting profile", {type: "error"})
+      console.log(error)
+  }
+  }
+return (
+  <div>
+      <button className='deletebtn' onClick={handleDelete}>Delete</button>
+  </div>
+)
+}
---
+++ tavernapp/src/components/firebase tilfoej copy.jsx
@@ -0,0 +1,120 @@
+import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import React, {useState} from 'react'
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage"
+import {storage, db} from "../firebaseConfigv2"
+import {toast} from "react-toastify"
+import "./AddProfile.css"
+
+export default function AddProfile() {
+    const [formData, setFormData] =useState({
+        title:"",
+        description:"",
+        image:"test",
+        createdAt: Timestamp.now().toDate(),
+    });
+
+    const [progress, setProgress] = useState(0);
+
+    const handleChange=(e)=>{
+        setFormData({...formData,[e.target.name]: e.target.value});
+        console.log(formData);
+    };
+
+    const handleChange2=(e)=>{
+        setFormData({...formData,[e.target.name]: e.target.value});
+        console.log(formData);
+    };
+
+    const handleImageChange=(e)=>{
+        setFormData({...formData,image:e.target.files[0]});
+    };
+
+    const handlePublish = ()=>{
+        if(!formData.title  || !formData.description){
+            alert("Please fill all the fields");
+            return;
+        }
+
+
+        const storageRef = ref(storage,`/images/${formData.image.name}` );
+
+        const uploadImage = uploadBytesResumable(storageRef,formData.image)
+
+        uploadImage.on("state_changed",
+        (snapshot)=>{
+            const progressPercent =Math.round((snapshot.bytesTransferred /snapshot.totalBytes)*100);
+            setProgress(progressPercent);
+        },
+        (err)=>{
+            console.log(err);
+        },
+        ()=>{
+            setFormData({
+                title: "",
+                description: "",
+                image: "test",
+            });
+            
+            getDownloadURL(uploadImage.snapshot.ref)
+            .then((url) => {
+                const ProfileRef = collection(db, "profiles");
+                console.log(formData.image);
+                if (formData.image==="test"){
+                    addDoc(ProfileRef, {
+                        title: formData.title,
+                        description: formData.description,
+                        createdAt: Timestamp.now().toDate(),
+                    })
+                    .then(() => {
+                        toast("Profile added successfully",{type:"success"});
+                        setProgress(0);
+                    })
+                    .catch((err) => {
+                        toast("Error adding Profile",{type:"error"});
+                });
+                } else {
+                addDoc(ProfileRef, {
+                    title: formData.title,
+                    description: formData.description,
+                    imageUrl:url,
+                    createdAt: Timestamp.now().toDate(),
+                })
+                .then(() => {
+                    toast("Profile added successfully",{type:"success"});
+                    setProgress(0);
+                })
+                .catch((err) => {
+                    toast("Error adding Profile",{type:"error"});
+            });}
+        });
+    });
+}
+  return (
+    <div className='form'>
+        <h2>Add a post</h2>
+        <label htmlFor="">Title</label>
+        <input type="text" name='title' className='form-control' value={formData.title} onChange={(e)=> handleChange(e)} />
+        
+        {/* description*/}
+        <label htmlFor="">description</label>
+        <textarea name='description' className='form-control' value={formData.description} onChange={(e)=> handleChange2(e)}/>
+
+        <label htmlFor="">Image</label>
+        <input type="file" name="image" accept="image/*" className="form-control" onChange={(e)=> handleImageChange(e)}/>
+
+{progress === 0 ? null :(
+    <div className="progess">
+        <div
+        className="progress-bar progress-bar-striped mt-2"
+        style={{width: `${progress}%` }}>
+            {`uploading image ${progress}%`}
+        </div>
+    </div>
+)}
+    
+        <button className='form-control-btn'onClick={handlePublish}>Publish</button>
+    </div>
+
+  )
+}
+
