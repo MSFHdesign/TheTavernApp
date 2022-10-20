5984ad5e00e6cce63092b420c3e05d7f298a4f3d
Michael <hovgaard1990@gmail.com>
2022-10-20 13:34:03 +0200

    Update Dices.jsx

--- tavernapp/src/components/Dices.jsx
+++ tavernapp/src/components/Dices.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
-import { Button, Box, Typography } from '@mui/material';
-
+import { Button, Box, Typography, } from '@mui/material';
+import SelectUnstyled from '@mui/base/SelectUnstyled';
+import OptionUnstyled from '@mui/base/OptionUnstyled';
 
 const App = () => {
   const [num, setNum] = useState(0);
@@ -12,14 +13,23 @@ const App = () => {
   
 
   const handleClick = () => {
-    setNum(randomNumberInRange(1, 80));
+    setNum(randomNumberInRange(1, 2));
   };
+ 
+
 
   return (
     <Box>
       <Typography variant ='h2'>number is: {num}</Typography>
       <Button onClick={handleClick}>Generate random number</Button>
-        <Input ></Input>
+    
+    <br/>
+
+      <SelectUnstyled className='Value' defaultValue={10}> 
+      <OptionUnstyled value={1}>{1}</OptionUnstyled>
+      <OptionUnstyled>{2}</OptionUnstyled>
+    </SelectUnstyled>
+
     </Box>
   );
 };
