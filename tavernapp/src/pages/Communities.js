344a007a80334abd91108259bb7c4e00a9e4ec5d
MultimedieGunvor <eaagupa@students.eaaa.dk>
2022-10-25 17:20:52 +0200

    localStorage på tag-filtrering

--- tavernapp/src/pages/Communities.js
+++ tavernapp/src/pages/Communities.js
@@ -1,101 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import {db} from "../firebaseConfig";
-import { Box } from '@mui/material';
-import './Communities.css';
+import React, { useState, useEffect } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import { Box } from "@mui/material";
+import "./Communities.css";
 
-import { Typography } from '@mui/material'
-
-
+import { Typography } from "@mui/material";
 
 export default function Communities() {
-    const [Communities, SetCommunities] = useState([
-        
-    ]);
+  const [Communities, SetCommunities] = useState([]);
 
-    function FancyButton ()  {
-        const [active, setActive] = useState(false);
-        const handleClick = () => {
-        setActive(!active);
-        };
+  function FancyButton(props) {    
+    const key = `${props.id}-is-active`;
 
-    return (
-        <div className="container">
-        <button className={ active ? "active" : "btn"} onClick={handleClick}>
-            { active ? "Følger" : "Følg"}
-        </button>
-        </div>
-        );
-    }
+    const [active, setActive] = useState(
+      JSON.parse(localStorage.getItem(key)) || false
+    );
 
-
-    const [state, setState] = useState(Communities);
-
-    const handleBtns =(e)=> {
-        let word=e.target.value 
-        if(word==='Alle') {
-            setState(Communities)
-        }
-        else if(word==='Class') {
-            const filtered = Communities.filter(item=>item.tags==='class');
-            setState(filtered);
-        }
-        else if(word==='Noobs') {
-            const filtered = Communities.filter(item=>item.tags==='noobs');
-            setState(filtered);
-        }
-        else if(word==='Accessories') {
-            const filtered = Communities.filter(item=>item.tags==='accessories');
-            setState(filtered);
-        }
-        else if(word==='Location') {
-            const filtered = Communities.filter(item=>item.tags==='location');
-            setState(filtered);
-            console.log(filtered);
-        } 
+    const handleClick = () => {
+      localStorage.setItem(key, !active);
+      setActive(!active);
     };
 
-    useEffect(()=>{
-        const communityRef = collection(db, "Communities");
-        const q = query(communityRef, orderBy("createdAt", "desc"));
-        onSnapshot(q,(snapshot) =>{
-            const communities = snapshot.docs.map((doc) =>({
-            id: doc.id,
-            ...doc.data(),}));
-            setState(communities);
-            SetCommunities(communities);
-            console.log(communities);
-        });
-        
-    },[]);
+    return (
+      <div className="container">
+        <button className={active ? "active" : "btn"} onClick={handleClick}>
+          {active ? "Følger" : "Følg"}
+        </button>
+      </div>
+    );
+  }
+
+  const [state, setState] = useState(Communities);
+
+  const handleBtns = (e) => {
+    let word = e.target.value;
+    if (word === "Alle") {
+      setState(Communities);
+    } else if (word === "Class") {
+      const filtered = Communities.filter((item) => item.tags === "class");
+      setState(filtered);
+    } else if (word === "Noobs") {
+      const filtered = Communities.filter((item) => item.tags === "noobs");
+      setState(filtered);
+    } else if (word === "Accessories") {
+      const filtered = Communities.filter(
+        (item) => item.tags === "accessories"
+      );
+      setState(filtered);
+    } else if (word === "Location") {
+      const filtered = Communities.filter((item) => item.tags === "location");
+      setState(filtered);
+      console.log(filtered);
+    }
+  };
+
+  useEffect(() => {
+    const communityRef = collection(db, "Communities");
+    const q = query(communityRef, orderBy("createdAt", "desc"));
+    onSnapshot(q, (snapshot) => {
+      const communities = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setState(communities);
+      SetCommunities(communities);
+      console.log(communities);
+    });
+  }, []);
 
   return (
-    <Box sx={{minHeight: '100vh', minWidth: '100vw',pt:'60px',pb:'60px', display: 'flex',flexDirection:'column', alignItems: 'center', justifyContent: 'space-evenly'}} className='communities'>
-        <Typography variant='h2' sx={{fontSize: 50, color: 'var(--H2Color)'}}>Communities</Typography>
-        <div className='btns'>
-            <button value="Alle" onClick={handleBtns}>Alle</button>
-            <button value="Class" onClick={handleBtns}>Class</button>
-            <button value="Noobs" onClick={handleBtns}>Ny i dnd</button>
-            <button value="Accessories" onClick={handleBtns}>Grej</button>
-            <button value="Location" onClick={handleBtns}>Nær mig</button>
-        </div>
-        {
-            Communities.length === 0?(
-                <p>Ingen communities at finde</p>
-            ):(
-            state.map((item)=>(
-                <div className='single-community' key={item.id} >
-                    <div className='text'>
-                         <div className={!item.imageUrl ? "noimg" : "img"}>
-                             <img src = {item.imageUrl} alt="title" />
-                         </div>
-                         <h2>{item.title}</h2>  
-                         <h4>{item.description}</h4>                           
-                         <FancyButton  />
-                     </div>
-                </div>
-                ))
-        )}
+    <Box
+      sx={{
+        minHeight: "100vh",
+        minWidth: "100vw",
+        pt: "60px",
+        pb: "60px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      }}
+      className="communities"
+    >
+      <Typography variant="h2" sx={{ fontSize: 50, color: "var(--H2Color)" }}>
+        Communities
+      </Typography>
+      <div className="btns">
+        <button value="Alle" onClick={handleBtns}>
+          Alle
+        </button>
+        <button value="Class" onClick={handleBtns}>
+          Class
+        </button>
+        <button value="Noobs" onClick={handleBtns}>
+          Ny i dnd
+        </button>
+        <button value="Accessories" onClick={handleBtns}>
+          Grej
+        </button>
+        <button value="Location" onClick={handleBtns}>
+          Nær mig
+        </button>
+      </div>
+      {Communities.length === 0 ? (
+        <p>Ingen communities at finde</p>
+      ) : (
+        state.map((item) => (
+          <div className="single-community" key={item.id}>
+            <div className="text">
+              <div className={!item.imageUrl ? "noimg" : "img"}>
+                <img src={item.imageUrl} alt="title" />
+              </div>
+              <h2>{item.title}</h2>
+              <h4>{item.description}</h4>
+              <FancyButton id={item.id} k={123} />
+            </div>
+          </div>
+        ))
+      )}
     </Box>
-  )
+  );
 }
