d982e8406e513c873fe29e6f24b925b3559a48bc
Michael <hovgaard1990@gmail.com>
2022-10-26 11:25:34 +0200

    Update fejl404.js

--- tavernapp/src/pages/fejl404.js
+++ tavernapp/src/pages/fejl404.js
@@ -4,6 +4,43 @@ import BottomNav from '../components/BottomNav';
 import TopNav from '../components/TopNav';
 export default function fejl404() {
 
+
+const Jokes = [
+
+  `How do you get a D&D player to go out with you?
+  You ask them for a d8`,
+  `Why did the Halfling stop dating his Warforged girlfriend?
+  Because she was too high maintenance`,
+  `How does the paladin protect against the heat of a Fire bolt?
+  He turns up his AC`,
+  `What forest animal helps the druid get that nice green colour for their robe?
+  A Dyer Wolf!`,
+  `What tool helps a wizard with writing the correct runes into their spellbook?
+  A Spell Checker!`,
+  `If the barbarian has ten gold pieces, and the rogue steals half of it, what does the rogue take?
+  1d12 slashing damage.`,
+  `What happens when you stand on a d4?
+  Your foot takes 1d4 damage.`,
+  `Where does the ranger keep their arrows?
+  In the monsters!`,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+]
+
+
   return (
     <Box sx={{minHeight: '100vh'}}>
          <TopNav/>
