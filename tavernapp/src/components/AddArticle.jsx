ea2e32a38ac1ef5ca304a57252f3ea25fe932335
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-10-26 09:27:52 +0200

    Update AddArticle.jsx

--- tavernapp/src/components/AddArticle.jsx
+++ tavernapp/src/components/AddArticle.jsx
@@ -11,16 +11,12 @@ export default function AddArticle() {
         description:"",
         image:"test",
         createdAt: Timestamp.now().toDate(),
+        community:"",
     });
 
     const [progress, setProgress] = useState(0);
 
     const handleChange=(e)=>{
-        setFormData({...formData,[e.target.name]: e.target.value});
-        console.log(formData);
-    };
-
-    const handleChange2=(e)=>{
         setFormData({...formData,[e.target.name]: e.target.value});
         console.log(formData);
     };
@@ -53,6 +49,7 @@ export default function AddArticle() {
                 title: "",
                 description: "",
                 image: "test",
+                community:"",
             });
             
             getDownloadURL(uploadImage.snapshot.ref)
@@ -64,6 +61,7 @@ export default function AddArticle() {
                         title: formData.title,
                         description: formData.description,
                         createdAt: Timestamp.now().toDate(),
+                        community: formData.community,
                     })
                     .then(() => {
                         toast("Article added successfully",{type:"success"});
@@ -78,6 +76,7 @@ export default function AddArticle() {
                     description: formData.description,
                     imageUrl:url,
                     createdAt: Timestamp.now().toDate(),
+                    community: formData.community,
                 })
                 .then(() => {
                     toast("Article added successfully",{type:"success"});
@@ -92,12 +91,25 @@ export default function AddArticle() {
   return (
     <div className='form'>
         <h2>Add a post</h2>
+        
+        <label htmlFor="">Select community</label>
+        <select name='Communities' className='form-control' value={formData.community} onChange={(e)=> handleChange(e)}>
+            <option value="DM-skærme">DM-skærme</option>
+            <option value="Dnd'ere i Aarhus">Dnd'ere i Aarhus</option>
+            <option value="Dnd'ere i Aalborg">Dnd'ere i Aalborg</option>
+            <option value="Guide til spændende karaktere">Guide til spændende karaktere</option>
+            <option value="For nybegyndere">For nybegyndere</option>
+            <option value="Terningsamlerne">Terningsamlerne</option>
+            <option value="Rangers be rangin">Rangers be rangin'</option>
+            <option value="Bards unite">Bards unite!</option>
+        </select>
+
         <label htmlFor="">Title</label>
         <input type="text" name='title' className='form-control' value={formData.title} onChange={(e)=> handleChange(e)} />
         
         {/* description*/}
         <label htmlFor="">description</label>
-        <textarea name='description' className='form-control' value={formData.description} onChange={(e)=> handleChange2(e)}/>
+        <textarea name='description' className='form-control' value={formData.description} onChange={(e)=> handleChange(e)}/>
 
         <label htmlFor="">Image</label>
         <input type="file" name="image" accept="image/*" className="form-control" onChange={(e)=> handleImageChange(e)}/>
