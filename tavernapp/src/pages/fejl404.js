7b3238f4cfe4206fadfcdb90c55dcadb6ec44704
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-10-26 12:13:56 +0200

    Joke generator

--- tavernapp/src/pages/fejl404.js
+++ tavernapp/src/pages/fejl404.js
@@ -1,67 +1,59 @@
-import { Box, Button, Typography } from '@mui/material'
-import React from 'react'
-import { useState } from 'react';
-import BottomNav from '../components/BottomNav';
-import TopNav from '../components/TopNav';
-export default function fejl404() {
+import { Box, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import BottomNav from "../components/BottomNav";
+import TopNav from "../components/TopNav";
+export default function Fejl404() {
+  const [test, setTest] = useState();
 
-
-
-const Jokes = [
-
-  `How do you get a D&D player to go out with you?
+  const Jokes = [
+    `How do you get a D&D player to go out with you?
   You ask them for a d8`,
-  `Why did the Halfling stop dating his Warforged girlfriend?
+    `Why did the Halfling stop dating his Warforged girlfriend?
   Because she was too high maintenance`,
-  `How does the paladin protect against the heat of a Fire bolt?
+    `How does the paladin protect against the heat of a Fire bolt?
   He turns up his AC`,
-  `What forest animal helps the druid get that nice green colour for their robe?
+    `What forest animal helps the druid get that nice green colour for their robe?
   A Dyer Wolf!`,
-  `What tool helps a wizard with writing the correct runes into their spellbook?
+    `What tool helps a wizard with writing the correct runes into their spellbook?
   A Spell Checker!`,
-  `If the barbarian has ten gold pieces, and the rogue steals half of it, what does the rogue take?
+    `If the barbarian has ten gold pieces, and the rogue steals half of it, what does the rogue take?
   1d12 slashing damage.`,
-  `What happens when you stand on a d4?
+    `What happens when you stand on a d4?
   Your foot takes 1d4 damage.`,
-  `Where does the ranger keep their arrows?
+    `Where does the ranger keep their arrows?
   In the monsters!`,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-  ``,
-]
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+    // ``,
+  ];
+  var math = 0;
 
-const [joke, setJoke] = useState(0);
-
-var math = 0;
-
-
-function randomNumberInRange() {
-    
-  var math = Math.floor(Math.random() * Jokes.length);
-};
+  function randomNumberInRange() {
+    math = Math.floor(Math.random() * Jokes.length);
+    setTest(Jokes[math]);
+  }
 
   return (
-    <Box sx={{minHeight: '100vh'}}>
-         <TopNav/>
-        <Typography variant="h3" color="initial">Det er menneskeligt at fejle</Typography>
-    
-<Button> HER ER EN KNAP </Button>
-    <Typography onClick={randomNumberInRange()}>
-      HER ER EN JOKE
-    </Typography>
-      
-  <BottomNav /> </Box>
-  )
+    <Box sx={{ minHeight: "100vh" }}>
+      <TopNav />
+      <Typography variant="h3" color="initial">
+        Det er menneskeligt at fejle
+      </Typography>
+      <Button onClick={randomNumberInRange}> HER ER EN KNAP </Button>
+      <Typography>{test}</Typography>
+      <BottomNav />
+    </Box>
+  );
 }
